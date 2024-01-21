8c86b29749645d017920da458e8552fbfcdc87bc
Jerry Zhang <hydrogen.studio.llc@gmail.com>
2024-01-21 12:10:45 -0800

    0.0.1-beta.3

--- main.js
+++ main.js
@@ -129,6 +129,7 @@ autoUpdater.on('download-progress', (progressObj) => {
 })
 autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded');
+  autoUpdater.quitAndInstall()
 });
 
 /**
