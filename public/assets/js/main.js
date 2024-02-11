bc4505c9930a478c4f617f3930e1e9c905fd5160
Jerry Zhang <hydrogen.studio.llc@gmail.com>
2024-02-11 09:01:54 -0800

    updated explanation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -100,9 +100,8 @@ if (title == "action monitor"){
 
   // Update wording to suit type
   let time = toTimestamp(days["startTime"]);
-  let endTime = toTimestamp(days["endTime"]);
 
-  // customize wording to match type
+  let endTime = toTimestamp(days["endTime"]);
   if(days.type == "tour"){
     if(document.getElementById("liveExplanation"))document.getElementById("liveExplanation").innerText = `The next tour date will start at ${time.toDateString()} ${time.toLocaleTimeString()} and will end at ${endTime.toDateString()} ${endTime.toLocaleTimeString()}`
     if(document.getElementById("eventInfoTitle"))document.getElementById("eventInfoTitle").innerText = "Next Stop's Information"
