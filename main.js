a60ad1a1e2ec44865157e13f27046aaf5dc7e3d6
Jerry Zhang <hydrogen.studio.llc@gmail.com>
2024-02-26 15:33:56 -0800

    added stuff

--- main.js
+++ main.js
@@ -110,14 +110,11 @@ app.whenReady().then(async () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0){
-      mainWindow = undefined;
       createWindow();
     }else{
       createWindow();
     }
   })
-
-  notify("Test", "test")
 
 })
 
